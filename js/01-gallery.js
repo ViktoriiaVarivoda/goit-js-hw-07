999a8d3b77a089c97eb31bd88ba4027944adeb0f
ViktoriiaVarivoda <victusia87@gmail.com>
2022-08-15 15:13:31 +0300

    задание 1_пункт 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createColorCardsGallery(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 
-// console.log(galleryItems);
+function createColorCardsGallery(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+            alt="${description}"
+            />
+            </a>
+            </div>`;
+        })
+        .join('');
+}
+
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function onGalleryContainerClick(evt) {
+    const isGalleryEl = evt.target.classList.contains('gallery__image');
+    if (!isGalleryEl) {
+        return;
+    }
+    console.log(evt.target.dataset.source);   
+    }
