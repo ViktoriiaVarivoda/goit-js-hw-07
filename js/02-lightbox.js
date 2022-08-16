2fdbbc1f3ca647bceb185b12b093cfd1195ad96b
ViktoriiaVarivoda <victusia87@gmail.com>
2022-08-16 12:39:10 +0300

    задание 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,37 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createColorCardsGallery(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createColorCardsGallery(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `<li class="gallery__item">
+            <a class="gallery__item" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+            alt="${description}"
+            />
+            </a>
+            </li>`;
+        })
+        .join('');
+}
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function onGalleryContainerClick(evt) {
+    const isGalleryEl = evt.target.classList.contains('gallery__image');
+    if (!isGalleryEl) {
+        return;
+    }
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+        captionsData: "alt",
+        captionsDelay: 250,
+    });
